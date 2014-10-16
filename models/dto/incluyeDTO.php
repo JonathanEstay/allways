149bfbff01df9e17489b0f423e528b22715321aa
Jonathan Estay <j.estay1988@gmail.com>
2014-10-16 18:15:09 -0300

    Creando DTO incluye

---
+++ models/dto/incluyeDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * Proyecto : Allways
+ * Autor    : Tsyacom Ltda.
+ * Fecha    : Martes, 4 de marzo de 2014
+ */
+
+class incluyeDTO
+{
+    private $_nombre;
+    
+    public function getNombre()
+    {
+        return $this->_nombre;
+    }
+    public function setNombre()
+    {
+        
+    }
+}
